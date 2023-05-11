d19765d1a37223671e1288316b3876be6358373b
mamun7025 <mamun7025@gmail.com>
2023-05-12 00:03:53 +0400

    Initial commit

---
+++ src/main/java/com/mamunrs/servicecore/ServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mamunrs.servicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCoreApplication.class, args);
+	}
+
+}
