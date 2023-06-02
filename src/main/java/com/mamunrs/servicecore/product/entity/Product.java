1a0c193469312fc92c670d2c1c7ca5cec78ba49f
mamun7025 <mamun7025@gmail.com>
2023-06-03 01:22:08 +0400

    add swagger API docs

--- src/main/java/com/mamunrs/servicecore/product/entity/Product.java
+++ src/main/java/com/mamunrs/servicecore/product/entity/Product.java
@@ -3,6 +3,8 @@ package com.mamunrs.servicecore.product.entity;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Setter
@@ -10,6 +12,7 @@ import javax.persistence.Id;
 @Entity
 public class Product {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     Long id;
     String code;
